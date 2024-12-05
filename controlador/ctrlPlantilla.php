124e13fda7bb4a88235109aeb1072019ebcd9d8a
Damaris <154326835+DamiMocch@users.noreply.github.com>
2024-12-05 01:17:42 -0600

    Add files via upload

---
+++ controlador/ctrlPlantilla.php
@@ -0,0 +1,15 @@
+<?php
+
+class Plantilla{
+
+    static public function ctrlPlantilla(){
+        include "vista\plantilla.php";
+    }
+
+    static public function ctrlRuta(){
+        return "http://localhost/Practica4_Damaris_Moctezuma/vista/recursos";
+    }
+
+}
+
+?>
